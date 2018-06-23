92d4496e41cf449db9ca626fe09d811624f2e19c
Johnson, David <David.X.Johnson@asurion.com>
2018-06-23 14:43:59 -0500

    moved mapstore code to its own repo

--- infrastructure/runtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-htmlout=/tmp/coverage.html
-coverout=/tmp/coverage.out
-
-go test -v -coverprofile=$coverout
-go tool cover -html=/tmp/coverage.out  -o $htmlout
-echo "HTML output written to $htmlout"
