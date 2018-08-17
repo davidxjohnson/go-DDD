f1ce39e347ad6c48aa86ca0303092a0e26e52d15
joseph <joseph@epiphyte.int.asurion.com>
2018-08-17 08:19:58 -0500

    modified the customer struct to contain fields necessary for this object to be fully fleshed out

--- customer/customer.go
+++ customer/customer.go
@@ -1,7 +1,11 @@
 package customer
 
 type Customer struct {
-	name  string
-	phone int
-	
+	name         string
+	phone        int
+	streetNumber int
+	streetName   string
+	city         string
+	state        string
+	country      string
 }
