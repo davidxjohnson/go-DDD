33f8ed9bc64492465b4671aa9701532736e1a6f8
joseph <joseph.shelby@asurion.com>
2018-09-14 17:47:13 +0000

    implemented a product constructor along with a test to validate its functionality. based this off of the constructor and test for creating a new customer

--- product/product.go
+++ product/product.go
@@ -1,8 +1,27 @@
 package product
 
+import (
+	"go-DDD/utility"
+)
+
 type Product struct {
 	id            string
 	shortName     string
 	description   string
 	price         float64
+}
+
+func (p Product) New(
+	shortName   string,
+	description string,
+	price       float64) (newProduct Product, err error) {
+
+	p.id          = utility.Makeuuid()	
+	p.shortName   = shortName
+	p.description = description
+	p.price       = price
+
+	newProduct = p
+
+	return 
 }
