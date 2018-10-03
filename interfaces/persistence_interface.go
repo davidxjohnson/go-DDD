d50052b09a655c98682d7fa7dcd406074b66bfc7
jshelby <joseph.shelby@asurion.com>
2018-10-03 10:17:46 -0500

    changed the interface name from state to persisted so that it would read 'an object of type persisted', implying the object is intended to bepersisted. still feel like this could be more explicitly named but is better than the previous iteration

--- interfaces/persistence_interface.go
+++ interfaces/persistence_interface.go
@@ -12,17 +12,19 @@ import (
 
 //This interface ensures that any type conforming to the state spec must implement
 //a method capable of persisting data and one capable of retrieving data 
-type state interface {
+type persisted interface {
 	persistState(po purchaseorder.PurchaseOrder) (string, error)
 }
 
+//This is a helper function for generating UUIDs to be used as index values for persisted
+//data
 func generateUUID() string {
 	return (uuid.Must(uuid.NewV4())).String()
 }
 
 //This function takes in a object of type PurchaseOrder for type safety, then encodes it as
-//JSON. This provides a []byte which we can then stream/write/etc as needed for persistance
-//It returns a string which is the index UUID so that the object can be retrieved later
+//JSON. This returns a []byte which we can then stream/write/etc as needed for persistance.
+//It returns a string to be used an index id so the object can be retrieved later
 func persistState(po purchaseorder.PurchaseOrder) (string, error) {
 	var (
 		fileId      = generateUUID()
